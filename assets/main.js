e53a7f4c17be454adb82f66ca225400a6c05ceee
vivianraquelly <160138824+vivianraquelly@users.noreply.github.com>
2024-11-04 11:44:55 -0300

    Add files via upload

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,17 @@
-function criarCartao(tema,pergunta,resposta){
-    
+function criaCartao(tema,pergunta,resposta){
+    const container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML = `
+    <div class="cartao-conteudo">
+    <h3>${tema}</h3>
+    <div class="cartao-conteudo-pergunta">
+        ${pergunta}
+    </div>
+    <div class="cartao-conteudo-resposta">
+    ${resposta}
+    </div>
+    `
+    container.appendChild(cartao)
 }
+criaCartao('Português','O que é uma paroxitona?','Uma paroxitona é uma palavra cuja sílaba tônica é penúltima')
